df6dc5c9104be92a41406b4dec520175c782b34f
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2017-09-11 22:51:08 +0530

    working registration

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -13,9 +13,9 @@ User = get_user_model()
 
 class UserRegisterSerializer(ModelSerializer):
     email = EmailField(label = 'Email')
-    email2 = EmailField(label = 'Confirm Email') #to confirm email
-    # password = CharField(style={'input-type' : 'password'}, label='Password')
-    # password2 = CharField(style={'input-type' : 'password'}, label='Confirm Password') #to confirm password
+    email2 = EmailField(label = 'Confirm Email', write_only=True) #to confirm email
+    password = CharField(style={'input-type' : 'password'}, label='Password', write_only=True)
+    password2 = CharField(style={'input-type' : 'password'}, label='Confirm Password', write_only=True) #to confirm password
     class Meta:
         model = User
         fields = [
@@ -23,64 +23,59 @@ class UserRegisterSerializer(ModelSerializer):
             'email',
             'email2',
             'password',
-            # 'password2',
+            'password2',
         ]
-        extra_kwargs = {
-            'password' : {'write_only' : True},  #passwords write only
-            # 'password2' : {'write_only' : True}
-            # 'email2' : {'write_only' : True }
-        }
 
-        def validate(self, data):  # check if given email already exists
-            email = data['email']
-            qs = User.objects.filter(email=email)
-            if qs.exists():
-                raise ValidationError("This email already exists, try again with a new email.")
-            return data
+    def validate(self, data):  # check if given email already exists
+        email = data['email']
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise ValidationError("This email already exists, try again with a new email.")
+        return data
 
-        def validate_email(self, value):
-            data = self.get_initial()
-            email1 = data.get("email2")
-            email2 = value
-            if email1 != email2:
-                raise ValidationError("Emails must match")
-            return value
+    def validate_email(self, value):
+        data = self.get_initial()
+        email1 = data.get("email2")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match")
+        return value
 
-        def validate_email2(self, value):
-            data = self.get_initial()
-            email1 = data.get("email")
-            email2 = value
-            if email1 != email2:
-                raise ValidationError("Emails must match")
-            return value
+    def validate_email2(self, value):
+        data = self.get_initial()
+        email1 = data.get("email")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match")
+        return value
 
-        # def validate_password(self, value):  #check if password matches confirm password
-        #     data = self.get_initial()
-        #     password1 = data.get("password2")
-        #     password2 = value
-        #     if password1 != password2:
-        #         raise ValidationError("Passwords must match.")
-        #     return value
-        #
-        # def validate_password2(self, value): #check if confirm password matches password
-        #     data = self.get_initial()
-        #     password1 = data.get("password")
-        #     password2 = value
-        #     if password1 != password2:
-        #         raise ValidationError("Passwords must match.")
-        #     return value
+    def validate_password(self, value):  #check if password matches confirm password
+        data = self.get_initial()
+        password1 = data.get("password2")
+        password2 = value
+        if password1 != password2:
+            raise ValidationError("Passwords must match.")
+        return value
 
-        def create(self, validated_data):
-            username = validated_data["username"]
-            email = validated_data["email"]
-            password = validated_data["password"]
-            user_obj = User(         #create user constructor with username and email
-                username = username,
-                email = email,
-            )
-            user_obj.set_password(password)  #set password
-            user_obj.save()
-            return validated_data
+    def validate_password2(self, value): #check if confirm password matches password
+        data = self.get_initial()
+        password1 = data.get("password")
+        password2 = value
+        if password1 != password2:
+            raise ValidationError("Passwords must match.")
+        return value
+
+    def create(self, validated_data):
+        username = validated_data["username"]
+        email = validated_data["email"]
+        password = validated_data["password"]
+        user_obj = User(         #create user constructor with username and email
+            username = username,
+            email = email,
+        )
+        user_obj.set_password(password)  #set password
+        user_obj.save()
+        return validated_data
 
 class UserLoginSerializer(ModelSerializer):
     username = CharField(required=False, allow_blank=True)  #not required field
