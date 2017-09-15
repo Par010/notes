651e22cba4fb878183e80f6e591efe0a24f264ef
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2017-09-15 19:42:03 +0530

    pagination implemented

--- note/api/views.py
+++ note/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import NoteSerializer
 from note.models import Note
 from .permissions import IsOwner
+from .pagination import NotePageNumberPagination
 from rest_framework.response import Response
 # from django.db.models import Q
 from rest_framework.permissions import(
@@ -18,6 +19,7 @@ from rest_framework.filters import (
 
 class NoteViewSet(ModelViewSet):
     serializer_class = NoteSerializer
+    pagination_class = NotePageNumberPagination
     permission_classes = [IsOwner]
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'content_plain', 'tags']
@@ -32,6 +34,10 @@ class NoteViewSet(ModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = NoteSerializer(queryset, many=True, context={'request': request})
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         return Response(serializer.data)
 
     def get_queryset(self):
